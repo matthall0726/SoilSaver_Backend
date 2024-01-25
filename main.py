1b1a58e5e9d67a1548e6c7e27853fde760c9c5a8
Matt Hall <matthall@Matts-MacBook-Pro.local>
2024-01-25 03:13:28 -0600

    Checking in base files

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Hello World")
+# import os
+# import subprocess
+#
+# # GitHub repository URL
+# repo_url = "https://github.com/your_username/your_repository.git"
+#
+# # Directory to clone the repository into
+# project_directory = "your_project_directory"
+#
+# # Clone the GitHub repository
+# subprocess.run(["git", "clone", repo_url, project_directory])
+#
+# # Change to the project directory
+# os.chdir(project_directory)
+#
+# # Install requirements from requirements.txt
+# subprocess.run(["pip", "install", "-r", "requirements.txt"])
+#
+# # Make main.py executable
+# subprocess.run(["chmod", "+x", "main.py"])
+#
+# # Run main.py
+# subprocess.run(["./main.py"])
