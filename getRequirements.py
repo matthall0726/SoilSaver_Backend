04cbbee88ba45e0d86a2abd4bb039ff7acf2dccc
Matt Hall <matthall@Matts-MacBook-Pro.local>
2024-01-25 23:13:49 -0600

    editing requirement script

--- getRequirements.py
+++ getRequirements.py
@@ -9,11 +9,11 @@ repo_url = "https://github.com/matthall0726/SoilSaver_Backend"
 project_directory = "SoilSaver_Backend"
 
 # Clone the GitHub repository
-subprocess.run(["git", "clone", repo_url, project_directory])
+subprocess.run(["git", "pull", repo_url])
 
-# Change to the project directory
-os.chdir(project_directory)
-print(subprocess.run(["pwd"]))
+# # Change to the project directory
+# os.chdir(project_directory)
+# print(subprocess.run(["pwd"]))
 
 # Install requirements from requirements.txt
 subprocess.run(["pip3", "install", "--upgrade", "-r", "requirements.txt"])
