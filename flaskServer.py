6d41f5d0a5182706d12ba942f2c5227eb580c2df
Matt Hall <matthall@Matts-MacBook-Pro.local>
2024-01-30 23:30:06 -0600

    updating order of function

--- flaskServer.py
+++ flaskServer.py
@@ -8,25 +8,33 @@ def setWifi(SSID, PASSWORD):
     COUNTRY_CODE = "US"
 
     # Path to wpa_supplicant.conf file on the Raspberry Pi's boot partition
-    wpa_supplicant_path = "/path/to/mounted/boot/partition/wpa_supplicant.conf"
+    wpa_supplicant_path = "/etc/wpa_supplicant/wpa_supplicant.conf"
 
     # Create or update wpa_supplicant.conf
     with open(wpa_supplicant_path, "w") as wpa_file:
         wpa_file.write(f"""country={COUNTRY_CODE}
-    ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-    update_config=1
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
 
-    network={{
-        ssid="{SSID}"
-        psk="{PASSWORD}"
-    }}
-    """)
+network={{
+    ssid="{SSID}"
+    psk="{PASSWORD}"
+}}
+""")
 
-    print("Wi-Fi credentials updated successfully. Rebooting...")
+    print("Wi-Fi credentials updated successfully.")
 
-    # Reboot the Raspberry Pi
-    os.system("sudo reboot")
+    # Remove access point configurations
+    os.system("sudo sed -i '/interface=wlan0/,/auth_algs=1/d' /etc/dhcpcd.conf")
+    os.system("sudo sed -i '/interface=wlan0/,/dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h/d' /etc/dnsmasq.conf")
+    os.system("sudo sed -i '/interface=wlan0/,/ssid=POOPYBUTT/d' /etc/hostapd/hostapd.conf")
+    os.system("sudo sed -i 's/DAEMON_CONF=\"\/etc\/hostapd\/hostapd.conf\"/#DAEMON_CONF=\"\"/' /etc/default/hostapd")
 
+    # Restart services
+    os.system("sudo service dhcpcd restart")
+    os.system("sudo systemctl restart hostapd")
+    os.system("sudo systemctl restart dnsmasq")
+    print("Access point configurations removed. Services restarted.")
 
 @app.route('/')
 def hello_world():
